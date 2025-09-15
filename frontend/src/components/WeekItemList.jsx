e7b3b69e0b3516e7b32f075f34c2cff01e31d462
Liviu Deleanu <livius0005@gmail.com>
2025-09-15 20:25:57 +0200

    Día 14 de mi desarrollo de mi calories-app.

--- frontend/src/components/WeekItemList.jsx
+++ frontend/src/components/WeekItemList.jsx
@@ -1,17 +1,21 @@
 import { useSelector } from "react-redux";
 import { selectWeekCalories } from "../redux/selectors/weekSelectors";
 
+import { daysOfIsoWeek } from "../utils/days";
+
 const WeekItemList = ({ week }) => {
   //Cambiar number_week por numberWeek
   const totalCalories = useSelector((state) =>
     selectWeekCalories(state, week.days),
   );
+  const days = daysOfIsoWeek(week.number_week, 2025, "DDMMYYYY");
+
   return (
     <>
       <h2>
         Week {week.number_week} <strong>({totalCalories} kcal)</strong>
       </h2>
-      <p>(semana en concreto: ejemplo -- 25/08/25 - 31/08/25)</p>
+      <p>{`(${days[0]} - ${days[6]})`}</p>
     </>
   );
 };
--- frontend/src/components/WeekSearch.jsx
+++ frontend/src/components/WeekSearch.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import useWeeks from "../hooks/useWeeks";
+
+import { weekToday } from "../utils/days";
+
 import WeekList from "./WeekList";
 import WeekForm from "./WeekForm";
 
@@ -7,6 +10,7 @@ const WeekSearch = () => {
   //Se obtienen las funciones de useWeeks y se pasan como props porque si uso el useWeeks en cada componente por separado, como el useWeeks tiene un useEffect dentro que hace un get a la api de todas las semanas con todos los datos, se haría una sobrecarga innecesaria de llamadas a la api. En cambio, en WeekItem, los subcomponentes que usa, esos mismos llaman individualmente a las funciones del custom hook useDays, porque ese custom hook NO usa un useEffect para hacer un GET automático.
   const { /*get,*/ create, del, loading } = useWeeks();
   const [search, setSearch] = useState("");
+  const [showWeekToday, setShowWeekToday] = useState(false);
   return (
     <>
       <form>
@@ -16,6 +20,7 @@ const WeekSearch = () => {
           value={search}
         />
       </form>
+
       {/*<button onClick={weeks?.length === 0 ? get : undefined} type="button">
         Reintentar
       </button>*/}
@@ -27,6 +32,10 @@ const WeekSearch = () => {
         <WeekList filter={search} del={del} />
       )}
       <WeekForm create={create} />
+      <button onClick={() => setShowWeekToday(!showWeekToday)}>
+        Consult the current week
+      </button>
+      {showWeekToday && <p>Week {weekToday()}</p> /*Esto se puede mejorar */}
     </>
   );
 };
